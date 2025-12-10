fc91df294c6e0a6fd0fd342c10765d99f7455c50
atharva <shivay@Atharvas-MacBook-Pro-3.local>
2025-12-10 01:44:54 +0100

    Added Frontend and Backend setup

---
+++ rabbitpark-backend/src/main/java/com/mds/rabbitpark/RabbitparkApplication.java
@@ -0,0 +1,13 @@
+package com.mds.rabbitpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitparkApplication.class, args);
+	}
+
+}
---
+++ rabbitpark-backend/src/test/java/com/mds/rabbitpark/RabbitparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mds.rabbitpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
